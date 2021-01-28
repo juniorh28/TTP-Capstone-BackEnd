2ba74b3160b7142a06540c7ccbfd649c6303bd4a
Cory Campbell <campbellcory2360@gmail.com>
2021-01-28 11:20:24 -0500

    Setup test database for map

--- db/db.js
+++ db/db.js
@@ -1,13 +1,20 @@
-const Sequelize = require("sequelize");
-const { name } = require("../package.json");
-require("dotenv").config();
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-// Initialize database with Sequelize
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
-  {
-    logging: false,
-  }
-);
+sequelize = new Sequelize(process.env.DB_NAME, process.env.USER_NAME, process.env.PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+});
 
-module.exports = db;
+const testDatabase = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    }catch(err){
+        console.error('Unable to connect', err);
+    }
+}
+
+testDatabase();
+
+module.exports = { sequelize };
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const serverRun = () => {
 // {force:true} - drops current tables and places new empty tables
 //{alter:true} - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 
-const syncDb = () => db.sync();
+const syncDb = () => db.sequelize.sync();
 // Connects to //postgres://localhost:5432/dbname
 
 //Run server and sync DB
